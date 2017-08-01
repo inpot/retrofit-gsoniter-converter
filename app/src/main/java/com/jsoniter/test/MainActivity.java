bb2eee342408053e6badb92c340a503e39f6ee33
luoming <luoming@qp197.com>
2017-08-01 14:31:43 +0800

    update usage

--- app/src/main/java/com/jsoniter/test/MainActivity.java
+++ app/src/main/java/com/jsoniter/test/MainActivity.java
@@ -6,17 +6,27 @@ import android.util.Log;
 
 import com.jsoniter.JsonIterator;
 
+import java.util.List;
+
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import jsoniter.JsoniterConverterFactory;
 import model.TestEntity;
+import retrofit2.Retrofit;
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 
 public class MainActivity extends AppCompatActivity {
     String TAG = "MainActivity";
+    String baseUrl = "https://test.78x56.com/api/v1/";
     String test ="{\"rs\":true,\"code\":200}";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        testJsoniter2();
+        testJsoniter();
     }
 
     private void testJsoniter2() {
@@ -24,5 +34,8 @@ public class MainActivity extends AppCompatActivity {
         Log.i(TAG, "testJsoniter2: " + entity.toString());
     }
 
-
+    private void testJsoniter() {
+        // usage add converter
+        Retrofit retrofit = new Retrofit.Builder().addConverterFactory(new JsoniterConverterFactory()).addCallAdapterFactory(RxJava2CallAdapterFactory.create()).baseUrl(baseUrl).build();
+    }
 }
